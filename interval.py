50ec8f19755461d29423975059d8b32996fa72af
bozar <5583771+Bozar@users.noreply.github.com>
2025-12-31 12:41:58 +0800

    Calculate simple intervals.

--- interval.py
+++ interval.py
@@ -1,21 +1,30 @@
-#import
+import random as rd
 
 
 MAX_PROBLEM = 10
+PROBLEMS = [
+	'C', 'D', 'E', 'F', 'G', 'A', 'B',
+]
 
 
 def get_problem(p_index):
-	print(f'Problem {p_index}: ')
+	max_index = len(PROBLEMS) - 1
+	left = rd.randint(0, max_index)
+	right = left
+	while right == left:
+		right = rd.randint(0, max_index)
+	print(f'{p_index + 1}: {PROBLEMS[left]} - {PROBLEMS[right]}')
+	return left, right
 
 
-def verify_answer(count_right):
+def verify_answer(count_right, left, right):
 	answer = input('?: ')
 	if answer.isdigit():
 		answer = int(answer)
 	else:
 		answer = -1
 
-	if not _is_right(answer):
+	if not _is_right_answer(answer, left, right):
 		print('Wrong answer')
 	else:
 		count_right += 1
@@ -26,7 +35,15 @@ def get_result(count_right):
 	print(f'{count_right}/{MAX_PROBLEM}')
 
 
-def _is_right(answer):
-	if answer % 2 == 0:
-		return True
+def _is_right_answer(answer, left, right):
+	check = 0
+	if left < right:
+		check = right - left + 1
+		#print(f'{answer}: {check}')
+		return answer == check
+	elif left > right:
+		check = 9 - (left - right + 1)
+		#print(f'{answer}: {check}')
+		return answer == check
+	raise ValueError('Left == Right: {left}')
 	return False
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import interval as it
 
 def main():
 	count_right = 0
+	left, right = 0, 0
 	for i in range(0, it.MAX_PROBLEM):
-		it.get_problem(i)
-		count_right = it.verify_answer(count_right)
+		left, right = it.get_problem(i)
+		count_right = it.verify_answer(count_right, left, right)
 	it.get_result(count_right)
 
 
